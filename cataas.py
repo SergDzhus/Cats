028a8c2caf65a26d1afebc790b3907032c59fec2
SergDzhus <SDzhoos@gmail.com>
2024-10-15 12:21:57 +0300

    Добавили меню вместо кнопки.

--- cataas.py
+++ cataas.py
@@ -3,8 +3,6 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from pygame.display import update
-
 
 def load_image(url):
     try:
@@ -26,6 +24,10 @@ def set_image():
         label.image = img
 
 
+def exit_prog():
+    window.destroy()
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
@@ -33,8 +35,17 @@ window.geometry("600x520")
 label = Label()
 label.pack()
 
-update_button = Button(text="Обнови котика", command=set_image)
-update_button.pack()
+# update_button = Button(text="Обнови котика", command=set_image)
+# update_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit_prog)
 
 url = "https://cataas.com/cat"
 set_image()
