4e04d0939d9c8900760e178f04acd921fb41e58e
SergDzhus <SDzhoos@gmail.com>
2024-10-15 11:55:13 +0300

    Создали окно и виджеты.

--- cataas.py
+++ cataas.py
@@ -1,1 +1,24 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+
+def load_image():
+
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "https://cataaas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img) #установка картинки на метку
+    label.image = img
+
+window.mainloop()
