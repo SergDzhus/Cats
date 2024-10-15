f994d15f9c8fe34e9a68f34f1da6c94950cfbc9b
SergDzhus <SDzhoos@gmail.com>
2024-10-15 12:07:36 +0300

    Добавили кнопку для загрузки нового изображения.

--- cataas.py
+++ cataas.py
@@ -3,6 +3,8 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+from pygame.display import update
+
 
 def load_image(url):
     try:
@@ -16,6 +18,13 @@ def load_image(url):
         return None
 
 
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image=img)  # установка картинки на метку
+        label.image = img
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
@@ -23,11 +32,10 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="Обнови котика", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img) #установка картинки на метку
-    label.image = img
+url = "https://cataas.com/cat"
+set_image()
 
 window.mainloop()
